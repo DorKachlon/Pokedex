ee0643d6da9d1ccdcf1fec86af6ac4fc2c708a4a
DorKachlon <67858139+DorKachlon@users.noreply.github.com>
2020-08-03 22:05:38 +0300

    final js tasks

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const searchButton = document.getElementById("searchButton");
 const searchInput = document.getElementById("search");
 const results = document.getElementById("results");
-
+const errorImage = document.getElementById("error");
+const divTypes = document.getElementById("divTypes");
+const divTypesNames = document.getElementById("divTypesNames");
+errorImage.hidden = true;
 // const searchPokemon = async (pokemonId) => {
 //     const { data } = await axios.get(
 //         `http://pokeapi.co/api/v2/pokemon/${pokemonId}`
@@ -15,37 +18,74 @@ const results = document.getElementById("results");
 //     );
 // };
 
-searchButton.addEventListener("click", async () => {
-    const { data } = await axios.get(
-        `http://pokeapi.co/api/v2/pokemon/${searchInput.value}`
-    );
-    makeDiv(
-        data.name,
-        data.height,
-        data.weight,
-        data.sprites.front_default,
-        data.sprites.back_default
-    );
+searchButton.addEventListener("click", () => {
+    searching(searchInput.value);
 });
+
+async function searching(id) {
+    try {
+        errorImage.hidden = true;
+        const { data } = await axios.get(
+            `http://pokeapi.co/api/v2/pokemon/${id}`
+        );
+        makeDiv(
+            data.name,
+            data.height,
+            data.weight,
+            data.sprites.front_default,
+            data.sprites.back_default,
+            data.types
+        );
+    } catch (e) {
+        errorImage.hidden = false;
+        results.innerHTML="";
+        divTypes.innerHTML = "";
+        divTypesNames.innerHTML = "";
+        searchInput.value="";
+    }
+}
 
 searchInput.addEventListener("keyup", function (e) {
     if (e.keyCode === 13) {
-        searchPokemon(searchInput.value);
+        searching(searchInput.value);
     }
 });
 
-const makeDiv = (name, height, weight, front, over) => {
+const makeDiv = (name, height, weight, front, over, types) => {
     const htmlText = `
     <div class="pokemonContainer">
     <div>Name: ${name}</div>
     <div>height: ${height}</div>
     <div>weight: ${weight}</div>
-    <div>picture: <img id="pokemonImg" src="${front}" /></div> 
+    <div><img id="pokemonImg" src="${front}" /></div> 
     </div>`;
     results.innerHTML = htmlText;
     const pokemonImg = document.getElementById("pokemonImg");
     pokemonImg.onmouseover = () => (pokemonImg.src = over);
     pokemonImg.onmouseout = () => (pokemonImg.src = front);
+
+    divTypes.innerHTML = "";
+    for (let i = 0; i < types.length; i++) {
+        const type = document.createElement("div");
+        type.innerHTML = `type ${i + 1}: ${types[i].type.name}`;
+        type.onclick = () => {
+            axios.get(`${types[i].type.url}`).then((res) => {
+                divTypesNames.innerHTML = "";
+                for (let j = 0; j < res.data.pokemon.length; j++) {
+                    const typeName = document.createElement("li");
+                    typeName.innerHTML = res.data.pokemon[j].pokemon.name;
+                    divTypesNames.append(typeName);
+                    typeName.onclick = () => {
+                        searching(res.data.pokemon[j].pokemon.name);
+                        divTypes.innerHTML = "";
+                        divTypesNames.innerHTML = "";
+                        searchInput.value="";
+                    };
+                }
+            });
+        };
+        divTypes.append(type);
+    }
 };
 
 // const searching=()=>{
